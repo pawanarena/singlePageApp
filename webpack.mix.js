d5921e6c6994d024499c2a3f17b6dd763383046b
Pawan <pawan.arena@gmail.com>
2019-04-22 21:43:33 +0545

    setup vue with laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,20 @@
-const mix = require('laravel-mix');
+// const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +28,20 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .webpackConfig({
+         module: {
+             rules: [
+                 {
+                     test: /\.jsx?$/,
+                     exclude: /node_modules(?!\/foundation-sites)|bower_components/,
+                     use: [
+                         {
+                             loader: 'babel-loader',
+                             options: Config.babel()
+                         }
+                     ]
+                 }
+             ]
+         }
+     })
    .sass('resources/sass/app.scss', 'public/css');
